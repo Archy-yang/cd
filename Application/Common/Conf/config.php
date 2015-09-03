2a108e246e59bbeffdbfeb09180a34825ac69d36
archy <admin@example.com>
2015-09-03 23:25:47 +0800

    kk

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -40,10 +40,13 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
+    //'DB_HOST'   => '127.0.0.1', // 服务器地址
+    'DB_HOST'   => '121.199.63.75', // 服务器地址
     'DB_NAME'   => 'chongdong', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
+    //'DB_USER'   => 'root', // 用户名
+    'DB_USER'   => 'admin', // 用户名
+    //'DB_PWD'    => 'root',  // 密码
+    'DB_PWD'    => 'cff1981@wit',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => '', // 数据库表前缀
 
