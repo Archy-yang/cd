301625eb3d183f355f4551e75dd220d301053723
archy <admin@example.com>
2015-09-04 12:11:12 +0800

    college

--- Application/Home/Controller/CollegeController.class.php
+++ Application/Home/Controller/CollegeController.class.php
@@ -5,22 +5,20 @@ namespace Home\Controller;
 /**
  * @author archy
  */
-class TransitionController extends HomeController 
+class CollegeController extends HomeController 
 {
     public function showList()
-    {   
+    {
         $num = 15;
 
-        $transition = M('transition');
-        $count = $transition->where(array('is_delete' => 0))->count();
+        $college = M('college');
+        $count = $college->where(array('is_delete' => 0))->count();
 
         $page = new Page($count, $num);
         $img = M('image')->where(array('type' => 1))->find();
         $show = $page->show();
 
-        
-
-        $list = $transition->where(array('is_delete' => 0))
+        $list = $college->where(array('is_delete' => 0))
             ->limit($page->firstRow, $page->listRows)
             ->select();
 
@@ -28,14 +26,17 @@ class TransitionController extends HomeController
         $this->assign('show', $show);
         $this->assign('img', $img);
         $this->display();
+    
     }
 
     public function detail($id)
     {
-        $transition = M('transition');
+        $college = M('college');
 
-        $detail = $transition->where(array('id' => $id))->find();
+        $detail = $college->where(array('id' => $id))->find();
 
+        $this->assign('detail', $detail);
+        
         $this->display();
     }
 }
