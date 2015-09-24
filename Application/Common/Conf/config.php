1e1e26a84e776316dc0ea09660f4c65e50cbef43
archy <admin@example.com>
2015-09-24 20:42:37 +0800

    [feature]
    调整到测试数据库

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -40,10 +40,14 @@ return array(
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
-    'DB_HOST'   => '127.0.0.1', // 服务器地址
-    'DB_NAME'   => 'chongdong', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
+    #'DB_HOST'   => '127.0.0.1', // 服务器地址
+    'DB_HOST'   => 'ailvxing.gotoftp3.com', // 服务器地址
+    #'DB_NAME'   => 'chongdong', // 数据库名
+    'DB_NAME'   => 'ailvxing', // 数据库名
+    #'DB_USER'   => 'root', // 用户名
+    'DB_USER'   => 'ailvxing', // 用户名
+    #'DB_PWD'    => 'root',  // 密码
+    'DB_PWD'    => 'fhqgykthh',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => '', // 数据库表前缀
 
--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -16,4 +16,5 @@ define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'L"K)0AD%Z6@UNcs!MJ<9twYv8e?5om=>RWi#:Olg'); //加密KEY
 define('UC_DB_DSN', 'mysqli://root:root@127.0.0.1:3306/chongdong'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysqli://ailvxing:fhqgykthh@ailvxing.gotoftp3.com/ailvxing'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', ''); // 数据表前缀，使用Model方式调用API必须配置此项
